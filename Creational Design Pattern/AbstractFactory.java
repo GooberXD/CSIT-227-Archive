5b4bb19ec5c8aacb726ae524c8efd535f98491c8
GooberXD <hanzivanlhermosada@gmail.com>
2026-06-29 21:50:55 +0800

    add abfact

---
+++ Creational Design Pattern/AbstractFactory.java
@@ -0,0 +1,60 @@
+public abstract class AbstractFactory {
+   abstract Color getColor(String color);
+   abstract Shape getShape(String shape);
+}
+
+public class ShapeFactory extends AbstractFactory {
+	
+   @Override
+   public Shape getShape(String shapeType){
+   
+      if(shapeType == null){
+         return null;
+      }		
+      
+      if(shapeType.equalsIgnoreCase("CIRCLE")){
+         return new Circle();
+         
+      } else if(shapeType.equalsIgnoreCase("RECTANGLE")){
+         return new Rectangle();
+         
+      } else if(shapeType.equalsIgnoreCase("SQUARE")){
+         return new Square();
+      }
+      
+      return null;
+   }
+   
+   @Override
+   Color getColor(String color) {
+      return null;
+   }
+}
+
+public class ColorFactory extends AbstractFactory {
+	
+   @Override
+   public Shape getShape(String shapeType){
+      return null;
+   }
+   
+   @Override
+   Color getColor(String color) {
+   
+      if(color == null){
+         return null;
+      }		
+      
+      if(color.equalsIgnoreCase("RED")){
+         return new Red();
+         
+      } else if(color.equalsIgnoreCase("GREEN")){
+         return new Green();
+         
+      } else if(color.equalsIgnoreCase("BLUE")){
+         return new Blue();
+      }
+      
+      return null;
+   }
+}
